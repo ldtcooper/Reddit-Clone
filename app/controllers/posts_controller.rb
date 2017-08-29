d9baee464307ae9f65e79a374ef00c6ca1622b86
App Academy Student <student@appacademy.io>
2017-08-29 16:30:27 -0700

    fill in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,35 @@
 class PostsController < ApplicationController
 
   def new
-
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.sub_id = params[:sub_id]
+
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to sub_url(@post.sub_id)
   end
-  
+
   private
 
   def post_params
